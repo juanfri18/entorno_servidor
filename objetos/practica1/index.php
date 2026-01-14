c0865601f5270f283a39e8c69242ce5624992bad
Juanfri <juanfranciscocort@gmail.com>
2026-01-14 15:21:47 +0100

    añado bootstrap al repositorio y sigo con las actividades de objetos

--- objetos/practica1/index.php
+++ objetos/practica1/index.php
@@ -1,23 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>EcoTrans Auditoría</title>
 </head>
 <body>
     <form name="tabla" method="post" action="resultado.php">
-        <textarea name="datos_raw" id="datos_raw" placeholder="EL| bcn-1010x |Furgoneta Reparto|2019-05-10|15000#HI|mad2020y | Camion Logistica|2023-11-02|5500" required></textarea>
-        <br><br>km minimos<br>
-        <input name="minimo" type="text" placeholder="km Minimos"><br>
-        km maximos<br>
-        <input name="maximo" type="text" placeholder="km Maximos"><br>
         
-        <label type="checkbox" name="extras[]" value="GPS"></label><br>
-        <label type="checkbox" name="extras[]" value="Sensor de carril"></label><br>
-        <label type="checkbox" name="extras[]" value="Cámara 360"></label><br>
-        <label type="checkbox" name="extras[]" value="ABS"></label><br>
-        <input type="submit" value="Enviar"><br><br>
+        <textarea name="datos_raw" id="datos_raw" rows="5" cols="50" placeholder="EL| bcn-1010x |Furgoneta Reparto|2019-05-10|15000#HI|mad2020y | Camion Logistica|2023-11-02|5500" required></textarea>
+        <br><br>
+
+        km minimos<br>
+        <input name="km_min" type="number" placeholder="km Minimos"><br>
+        
+        km maximos<br>
+        <input name="km_max" type="number" placeholder="km Maximos"><br>
+        <br>
+        
+        Extras:<br>
+        <input type="checkbox" name="extras[]" value="GPS"> GPS<br>
+        
+        <input type="checkbox" name="extras[]" value="Sensor de carril"> Sensor de carril<br>
+        
+        <input type="checkbox" name="extras[]" value="Cámara 360"> Cámara 360<br>
+        
+        <input type="checkbox" name="extras[]" value="ABS"> ABS<br>
+        
+        <br>
+        <input type="submit" value="Enviar">
     </form>
 
 </body>
